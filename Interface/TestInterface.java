d466a072b26bbaeb431dacd8c713066bb2057cc4
alexweav <warriordude417@suddenlink.net>
2015-04-19 19:33:58 -1000

    Added documentation to Interface

--- Interface/TestInterface.java
+++ Interface/TestInterface.java
@@ -1,4 +1,5 @@
-
+//Alexander Weaver
+//Last update: 4-19-2015 7:30pm
 package Interface;
 
 import Primality.FermatPrimalityTester;
@@ -8,11 +9,15 @@ import Primality.SolovayStrassenPrimalityTester;
 
 import java.math.BigInteger;
 
+//Main class interface
+//Used for functionally testing pieces as they are made
+//Will not be used in final program, purely for build testing
 public class TestInterface {
     
     
 
     public static void main(String[] args) {
+        //Creates the testers
         FermatPrimalityTester ftester = new FermatPrimalityTester();
         SolovayStrassenPrimalityTester sstester = new SolovayStrassenPrimalityTester();
         FactorizationPrimalityTester reference = new FactorizationPrimalityTester();
@@ -20,6 +25,12 @@ public class TestInterface {
         
         System.out.println(mrtester.isPrime(11, 1));
         
+        //Set the numIterations to a positive integer
+        //This loops through the natural numbers, increasingly
+        //For each natural number, the value is tested with the SS primality test and with the factorization test (which is guaranteed accuracy)
+        //Stops when there is a difference (i.e. the SS primality test fails)
+        //Prints that number
+        //User can change the number of iterations in order to see the impact on accuracy
         int i = 2;
         int numIterations = 1;
         while(true) {
