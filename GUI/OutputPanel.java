1c67ffbdc5ae8e303dd9aca4dd81f341b9077db6
alexweav <alexander.weaver@ttu.edu>
2015-05-21 17:32:25 -0500

    Status pane added

--- GUI/OutputPanel.java
+++ GUI/OutputPanel.java
@@ -9,7 +9,14 @@ public class OutputPanel extends JPanel {
     
     public OutputPanel(JComponent parent, int width, int height) {
         this.setMaximumSize(new Dimension(width, height));
-        //outputPanel.setBackground(Color.white);
+        this.setLayout(new BorderLayout());
         parent.add(this);
+        JLabel label = new JLabel("Status:");
+        this.add(label, BorderLayout.PAGE_START);
+        JEditorPane textArea = new JEditorPane();
+        JScrollPane editorScrollPane = new JScrollPane(textArea);
+        editorScrollPane.setPreferredSize(new Dimension(500, 60));
+        editorScrollPane.setMinimumSize(new Dimension(500, 60));
+        this.add(editorScrollPane, BorderLayout.LINE_START);
     }
 }
