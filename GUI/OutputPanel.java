2ecc38342ed5bf4b24062ea2569ce23cb1a632b0
alexweav <alexander.weaver@ttu.edu>
2015-05-23 18:34:03 -0500

    Text pane & copy button functional

--- GUI/OutputPanel.java
+++ GUI/OutputPanel.java
@@ -1,11 +1,18 @@
 //Alexander Weaver
-//Last update: 5-21-2015 2:29pm
+//Last update: 5-23-2015 6:32pm
 package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class OutputPanel extends JPanel {
+public class OutputPanel extends JPanel implements ActionListener {
+    
+    private String statusText = "";
+    private JEditorPane textArea;
     
     public OutputPanel(JComponent parent, int width, int height) {
         this.setMaximumSize(new Dimension(width, height));
@@ -13,10 +20,33 @@ public class OutputPanel extends JPanel {
         parent.add(this);
         JLabel label = new JLabel("Status:");
         this.add(label, BorderLayout.PAGE_START);
-        JEditorPane textArea = new JEditorPane();
+        textArea = new JEditorPane();
         JScrollPane editorScrollPane = new JScrollPane(textArea);
-        editorScrollPane.setPreferredSize(new Dimension(500, 60));
-        editorScrollPane.setMinimumSize(new Dimension(500, 60));
-        this.add(editorScrollPane, BorderLayout.LINE_START);
+        textArea.setText(statusText);
+        editorScrollPane.setPreferredSize(new Dimension(450, 60));
+        editorScrollPane.setMinimumSize(new Dimension(450, 60));
+        this.add(editorScrollPane, BorderLayout.CENTER);
+        JButton clipboardButton = new JButton("Copy Status");
+        clipboardButton.setActionCommand("copyText");
+        clipboardButton.addActionListener(this);
+        this.add(clipboardButton, BorderLayout.LINE_END);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if("copyText".equals(e.getActionCommand())) {
+            System.out.println("copy");
+            StringSelection ss = new StringSelection(statusText);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(ss, null);
+        }
+    }
+    
+    public void setStatusText(String str) {
+        statusText = str;
+        textArea.setText(statusText);
+    }
+    
+    public String getStatusText() {
+        return statusText;
     }
 }
